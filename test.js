52fded0da7a74f4aa04edd0b13eb7c27c7ed28af
Diksha Shrivastav <DikshaShrivastav2424@gmail.com>
2025-11-26 11:20:28 +0530

    Failing test added

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ function add(a, b) {
   return a + b;
 }
 
-assert.strictEqual(add(2, 3), 5); // agar yaha galat expected value dogi to test fail ho jayega
+assert.strictEqual(add(2, 3), 4); // agar yaha galat expected value dogi to test fail ho jayega
 console.log("All tests passed!");
